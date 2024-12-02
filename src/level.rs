6ae0fb612f8b227c028b31e031d1577051e0deae
knight <karvin.jimenez@alternard.com>
2024-12-02 17:48:41 -0400

    working in part 2 solution.

--- src/level.rs
+++ src/level.rs
@@ -8,6 +8,8 @@ pub fn run() {
     let mut safe_list: Vec<&str> = Vec::new();
     let mut safe_levels_quantity = 0;
 
+    let is_part2 = false;
+
     levels.lines().for_each(|ids| {
 
         let mut is_safe = true;
@@ -20,6 +22,8 @@ pub fn run() {
         let second_level = levels_arrays[1].parse::<i32>().unwrap();
 
         let is_ascending = previous_level < second_level;
+
+        let mut unsafe_levels_quantity = 0;
 
         for level in levels_arrays.iter() {
 
@@ -43,6 +47,17 @@ pub fn run() {
             else if !is_ascending && actual_level < previous_level && level_gap.abs() < 4 {
 
                 previous_level = actual_level;
+            }
+            else if is_part2 {
+
+                unsafe_levels_quantity += 1;
+                previous_level = actual_level;
+
+                if unsafe_levels_quantity > 1 {
+
+                    is_safe = false;
+                    break;
+                }
             }
             else {
                 is_safe = false;
